99e3bb4d1a744e9e384f272a5fd807ef9480860c
APachon10 <Pachondestroy95@gmail.com>
2019-01-21 15:40:42 +0100

    Practica 1 casi Hecha faltan 2 Ejercicios( los ultimos)

---
+++ src/ejercicios/CMD.java
@@ -0,0 +1,60 @@
+package ejercicios;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class CMD {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);		
+		String comanda = "";
+		String confirmar = "";
+
+		String ruta = System.getProperty("user.dir");;
+		boolean salir = false,confi=false;
+
+		File f = new File(ruta);
+		File [] Raiz=f.listFiles();
+		System.out.print(System.getProperty("user.dir") + " $ ");
+		do {
+			comanda = sc.nextLine();
+			String com[] = comanda.split(" ");
+			System.out.println("------------------------------");
+			for(int i =0;i<Raiz.length; i++) {
+				if(comanda.contains(Raiz[i].getName())){
+					ruta = f.getPath() + File.separator + com[1];
+					f = new File (ruta);
+				}
+				if (comanda.contains("~")){
+					ruta = System.getProperty("user.dir");
+					f= new File (ruta);
+				}
+			}
+			if(comanda.equals("cd .." )){
+				ruta = f.getParent();
+				f = new File(ruta);
+			}
+			File carpeta []= f.listFiles();
+			Raiz = carpeta;
+			if(comanda.equals("dir")){
+				for(int x = 0;x<carpeta.length;x++){
+					System.out.println(carpeta[x].getName());
+				}
+			}
+			System.out.print(f.getAbsolutePath() + " $ ");
+			
+		}while(!comanda.equals("fi"));
+		
+		System.out.println();
+		if(comanda.equals("fi")) {
+			System.out.println("Quieres Cerrar la Terminal ? ");
+			confirmar = sc.next();
+			if(confirmar.equalsIgnoreCase("Si")) {
+				System.out.println("Terminal Cerrrado ");
+				System.out.println("Cerrando Programa ");
+			}
+		}else{
+			
+		}
+	}
+	}
+
---
+++ src/ejercicios/Crear_Borrar_Carpeta.java
@@ -0,0 +1,71 @@
+package ejercicios;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Crear_Borrar_Carpeta {
+	public static void main(String[] args) {
+		File f = new File("Temp");
+
+		if (!f.exists()) {
+			crearCarpeta(f);
+			System.out.println("Se ha creado la carpeta : "+f.getAbsolutePath());
+		}else {
+			System.out.println("La carpeta "+f.getName() + " Existe que quieres hacer con ella? ");
+			menu(f);
+			crearCarpeta(f);
+		}
+	}
+	public static void menu(File v) {
+		Scanner scan = new Scanner(System.in);
+		int op = 1;
+		
+		while(op!=0) {
+			System.out.println("1- Volverla a crear"
+					+ "\n2- Borrarla ");
+			op = scan.nextInt();
+			switch (op) {
+			case 1:
+				crearCarpeta(v);
+				break;
+			case 2:
+				borrarCarpeta(v);
+				break;
+			}
+		}
+		scan.close();
+	}
+	public static void crearCarpeta(File v ) {
+		
+		File[] archivos;
+		archivos = v.listFiles();
+		boolean salir=false;
+		
+		while(!salir){
+			if (archivos.length==0) {
+				v.mkdir();
+				salir=true;
+				System.out.println("Carpeta Creada ");
+			}else{
+				System.out.println("No se puede crearde nuevo porque la carpeta no esta vacia");
+				salir=true;
+			}
+		}
+		
+	}
+	public static void borrarCarpeta(File v ) {
+		File[] archivos;
+		archivos = v.listFiles();
+		boolean salir = false;
+
+		while(!salir){
+			if (archivos.length==0) {
+				v.delete();
+				salir=true;
+			}else{
+				System.out.println("No se ha borrado la carpeta porque existen archivos dentro de ella");
+				salir=true;
+			}
+		}
+	}
+}
