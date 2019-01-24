978c988c1aad5968283a7e16a8450a47685a3623
APachon10 <Pachondestroy95@gmail.com>
2019-01-24 16:35:21 +0100

    Practica 2 Ejercicio 1: Hecho

--- src/Practica2/calcularMediaAlumnops.java
+++ src/Practica2/calcularMediaAlumnops.java
@@ -11,37 +11,74 @@ public class calcularMediaAlumnops {
 		calcularMediaAlumnops c =new calcularMediaAlumnops();
 		c.calcularMedia(f);
 	}
-	public int calcularMedia(File v) {
-		int media = 0;
+	public void inici() {
+		double valores = 0;
+		int val =  0;
 		try {
-			Scanner scan = new Scanner(v);
-			boolean salir  =false;
-			while (!salir) {
-				String texto =  scan.nextLine();
-				String [] linea= texto.split("");
-				if (texto.equals("fi")) {
-					salir=true;
+			Scanner lector = new Scanner(f);
+			// Aquesta estrategia es basa en un semafor
+			boolean llegir = true;
+			// Si s'executa aquesta instrucci, s'ha obert el fitxer
+			while (llegir) {
+				String valor = lector.nextLine();
+				String [] pal = valor.split(" ");
+				if (valor == "fi") {
+					// Marca de finalitzaci  Ja s'ha acabat la lectura
+					llegir = false;
 				}else {
-					if (scan.hasNextDouble()) {
-						
-					}else {
-						scan.next();
+					double suma=0,media=0;
+					// Encara no s'ha acabat. Tractar dada
+					System.out.println(valor + " ");
+					System.out.println("--------------------");
+					for(int i =0 ; i<pal.length;i++) {
+						val= Integer.parseInt(pal[2]);
+						if(i>2) {
+							valores= Double.parseDouble(pal[i]);
+							System.out.println("nota--->"+valores);
+							suma = suma+valores;
+							media = suma /val;
+						}
 					}
+					System.out.println("Media  ---> " + media);
 				}
-				System.out.println("Vector--> " +Arrays.toString(linea));
 			}
-			
-			scan.close();
+			// Cal tancar el fitxer
+			lector.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Error: " + e);
 		}
-		return media; 
 	}
-	public static String leerString() {
-		Scanner scan = new Scanner(System.in);
-		String palabra = "";
-		palabra = scan.nextLine();
-		scan.close();
-		return palabra;
-	}
+	//	public double calcularMedia(File v) {
+	//		double media = 0,suma =0;
+	//		boolean salir = false;
+	//		try {
+	//			Scanner scan = new Scanner(v);
+	//			String texto = scan.next();
+	//			while (!salir) {
+	//				if (texto.equals("fi")) {
+	//					salir=true;
+	//				}else {
+	//					if (scan.hasNextInt()) {
+	//						System.out.println("Hola");
+	//						if (texto.equals("-1")) {
+	//							System.out.println();
+	//						}
+	//					}else {
+	//						scan.next();
+	//					}
+	//				}
+	//			}
+	//			scan.close();
+	//		} catch (Exception e) {
+	//			e.printStackTrace();
+	//		}
+	//		return media; 
+	//	}
+	//	public static String leerString() {
+	//		Scanner scan = new Scanner(System.in);
+	//		String palabra = "";
+	//		palabra = scan.nextLine();
+	//		scan.close();
+	//		return palabra;
+	//	}
 }
