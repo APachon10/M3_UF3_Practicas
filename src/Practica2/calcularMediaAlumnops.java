7749921864b5b63a3c23bf9decb10cb4d2ddd3ef
APachon10 <Pachondestroy95@gmail.com>
2019-01-24 12:10:58 +0100

    Practica 2 Ex1 : Casi Hecho

--- src/Practica2/calcularMediaAlumnops.java
+++ src/Practica2/calcularMediaAlumnops.java
@@ -9,37 +9,33 @@ public class calcularMediaAlumnops {
 		System.out.print("Que Fichero quieres Leer ? ");
 		File f = new File(leerString());
 		calcularMediaAlumnops c =new calcularMediaAlumnops();
-		
 		c.calcularMedia(f);
 	}
 	public int calcularMedia(File v) {
 		int media = 0;
-		int Notas[] = null;
 		try {
 			Scanner scan = new Scanner(v);
-			boolean salir  =true;
-			int i=0;
+			boolean salir  =false;
 			while (!salir) {
-				String texto =  scan.next();
+				String texto =  scan.nextLine();
+				String [] linea= texto.split("");
 				if (texto.equals("fi")) {
 					salir=true;
 				}else {
-					if (scan.hasNextInt()) {
-						Notas[i]=Integer.parseInt(texto);
-						mostrarVector(Notas);
-						i++;
-					}else{
-						i++;
+					if (scan.hasNextDouble()) {
+						
+					}else {
+						scan.next();
 					}
 				}
+				System.out.println("Vector--> " +Arrays.toString(linea));
 			}
+			
+			scan.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return media; 
-	}
-	public void mostrarVector(int [] vector ) {
-		System.out.println("Vector: "+ Arrays.toString(vector));
 	}
 	public static String leerString() {
 		Scanner scan = new Scanner(System.in);
