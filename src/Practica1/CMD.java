b3d05c4f6e956798c47e92a9f6bf69355ee0f9b0
Alberto Pachon <Pachondestroy95@gmail.com>
2019-01-27 21:17:56 +0100

    Ejercicio 5 Perfectamente Hecho

--- src/Practica1/CMD.java
+++ src/Practica1/CMD.java
@@ -3,58 +3,66 @@ package Practica1;
 import java.io.File;
 import java.util.Scanner;
 
-public class CMD {
+public class  CMD{
 	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);		
-		String comanda = "";
-		String confirmar = "";
-
-		String ruta = System.getProperty("user.dir");;
-		boolean salir = false,confi=false;
-
-		File f = new File(ruta);
-		File [] Raiz=f.listFiles();
-		System.out.print(System.getProperty("user.dir") + " $ ");
-		do {
-			comanda = sc.nextLine();
-			String com[] = comanda.split(" ");
-			System.out.println("------------------------------");
-			for(int i =0;i<Raiz.length; i++) {
-				if(comanda.contains(Raiz[i].getName())){
-					ruta = f.getPath() + File.separator + com[1];
-					f = new File (ruta);
-				}
-				if (comanda.contains("~")){
-					ruta = System.getProperty("user.dir");
-					f= new File (ruta);
-				}
-			}
-			if(comanda.equals("cd .." )){
-				ruta = f.getParent();
-				f = new File(ruta);
-			}
-			File carpeta []= f.listFiles();
-			Raiz = carpeta;
-			if(comanda.equals("dir")){
-				for(int x = 0;x<carpeta.length;x++){
-					System.out.println(carpeta[x].getName());
-				}
-			}
-			System.out.print(f.getAbsolutePath() + " $ ");
-			
-		}while(!comanda.equals("fi"));
+		String raiz = System.getProperty("user.dir");
+		CMD cd = new CMD();
+		cd.consola(raiz);
+	}
+	public void consola(String r) {
+		Scanner scan = new Scanner(System.in);
+		String rutaActual =r,rutaAnterior="";
 		
-		System.out.println();
-		if(comanda.equals("fi")) {
-			System.out.println("Quieres Cerrar la Terminal ? ");
-			confirmar = sc.next();
-			if(confirmar.equalsIgnoreCase("Si")) {
-				System.out.println("Terminal Cerrrado ");
-				System.out.println("Cerrando Programa ");
-			}
-		}else{
+		File f =new File(rutaActual);
+		File [] carpeta=f.listFiles();
+		rutaAnterior = f.getParent();
+		String texto = "";
+		while (!texto.equals("fi")) {
+			int i=0;
+			System.out.print(f.getPath()+"$:");
 			
+			texto =leerString();
+			String comando [] = texto.split(" ");
+			
+			if (texto.equalsIgnoreCase("dir")) {
+				mostrarc(carpeta);
+			}else if (texto.equalsIgnoreCase("fi")) {
+				System.out.println("Saliendo de la consola ");
+			}
+			
+			for(int j =0;i<carpeta.length-1; i++) {
+				if(texto.contains(carpeta[i].getName())){
+					rutaActual = f.getPath() + File.separator + comando[1];
+					f = new File (rutaActual);
+					carpeta =f.listFiles();
+				}else if (texto.contains("~")){
+					rutaActual = System.getProperty("user.dir");
+					f= new File (rutaActual);
+					carpeta =f.listFiles();
+				}
+			}
+			if(texto.equals("cd .." )){
+				rutaActual = f.getParent();
+				f = new File(rutaActual);
+				carpeta= f.listFiles();
+			}
+		}
+		System.out.println("Programa Terminado");
+	}
+	//Metodo para mostrar Ficheros 
+	public  void mostrarc(File [] c) {	 
+		for (int i = 0; i < c.length; i++) {
+			if(c[i].isFile()) {
+				System.out.println("[FILE] " + c[i].getName());
+			} else {
+				System.out.println("[DIR] " + c[i].getName());
+			}
 		}
 	}
+	/*Metodo para leer Cadenas */
+	public static String leerString () {
+		Scanner scan = new Scanner(System.in);
+		String palabra = scan.nextLine();
+		return palabra;
 	}
-
+}
