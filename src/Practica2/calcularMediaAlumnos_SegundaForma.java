59eb796b2db0576e1f88ee96d784d5034cd93a89
APachon10 <Pachondestroy95@gmail.com>
2019-01-28 10:24:39 +0100

    Hola

--- src/Practica2/calcularMediaAlumnos_SegundaForma.java
+++ src/Practica2/calcularMediaAlumnos_SegundaForma.java
@@ -1,6 +1,7 @@
 package Practica2;
 
 import java.io.File;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -9,49 +10,70 @@ public class calcularMediaAlumnos_SegundaForma {
 		System.out.print("Que Fichero quieres Leer ? ");
 		File f = new File(leerString());
 		calcularMediaAlumnos_SegundaForma c2 = new calcularMediaAlumnos_SegundaForma();
+		if (f.exists()) {
+			c2.leerFichero(f);
+		}else {
+			String confirmacion = leerString();
+			System.out.println("El Archivo " + f.getName() + " No existe quieres Crearlo ? ");
+			if (confirmacion.equalsIgnoreCase("si")) {
+				c2.escribirFichero(f);
+			}
+			c2.leerFichero(f);
+			
+		}	
 	}
-	public double calcularMedia(File v) {
-		double valores = 0,media=0,suma=0;
+	public void calcularMedia(File v,String [] linea) {
+		leerFichero(v);
+		double media=0,suma=0;
 		int cont=0;
+		
+		for (int i = 0; i < linea.length; i++) {						
+			if (isNumeric(linea[i])) {
+				double valores = Double.parseDouble(linea[i]);
+				cont++;
+				suma = suma+valores;
+				media = suma /cont;
+			}
+		}
+		System.out.println("Media --> "+media );
+		
+		
+	}
+	public void leerFichero(File v) {
 		try {
 			Scanner scan = new Scanner(v);
 			boolean salir  =false;
 			while (!salir) {
 				String texto =  scan.nextLine();
-				String [] linea= texto.split("");
-				if (texto.equals("fi")) {
+				String [] linea= texto.split(" ");
+				if (texto.equalsIgnoreCase("fi")) {
 					salir=true;
-				}else {
-					if (scan.hasNextDouble()) {
-						// Encara no s'ha acabat. Tractar dada
-						System.out.println(texto + " ");
-						System.out.println("--------------------");
-
-						for(int i =0 ; i<linea.length-1;i++) {
-							if(i>1) {
-								if(linea[i].equals("-1")){
-									System.out.println();
-								}else{
-									valores= Double.parseDouble(linea[i]);
-									cont++;
-									System.out.println("nota--->"+valores);
-									suma = suma+valores;
-									media = suma /cont;
-								}
-							}
-						}
-						System.out.println("--------------------------");
-						System.out.println("Suma --> "+suma);
-						System.out.println("Media  ---> " + media);
-						System.out.println("--------------------------");
-					}
 				}
-			}		
+				if (texto.equals("-1")) {
+					scan.nextLine();
+				}
+			}
 			scan.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return media; 
+	}
+	public void escribirFichero(File v ) {
+		try {
+			leerFichero(v);
+			PrintStream ps = new PrintStream(v);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static boolean isNumeric(String cadena) {
+	    try {
+	        double d = Double.parseDouble(cadena);
+	    } catch (NumberFormatException | NullPointerException nfe) {
+	        return false;
+	    }
+	    return true;
 	}
 	public static String leerString() {
 		Scanner scan = new Scanner(System.in);
