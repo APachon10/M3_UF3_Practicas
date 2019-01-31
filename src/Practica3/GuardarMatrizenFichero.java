fcb12e3c6ef5cb700cd943c379cfd13b3ed7d3fb
APachon10 <Pachondestroy95@gmail.com>
2019-01-31 17:08:28 +0100

    Practica 3 Ejercicio 5

--- src/Practica3/GuardarMatrizenFichero.java
+++ src/Practica3/GuardarMatrizenFichero.java
@@ -3,6 +3,7 @@ package Practica3;
 import java.io.File;
 import java.io.PrintStream;
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuardarMatrizenFichero {
 	public static void main(String[] args) {
@@ -10,9 +11,21 @@ public class GuardarMatrizenFichero {
 		//Constantes para conseguir la medida de la Matriz 
 		final int columnas = 7;
 		final int filas =10;
-		File f =new File("Matriz.txt");
+		//Le pedimos al usuario si quiere generar la Matriz a traves del fichero por defecto o generar una Aleatoria
+		//Para generar la Aleatoria introduce Aleatoria y para Generar la por defecto introduce Por Defecto
 		
-		gc.escribirMatriz(f,columnas,filas);
+		System.out.println("Quieres generar la Matriz a a partir del archivo por defecto o crear una Matriz aleatoria??");
+		String pregunta = recogerCadena();
+		
+		if (pregunta.equalsIgnoreCase("Aleatoria")) {
+			File archivo_creado = new File("Matriz.txt");
+			gc.escribirMatriz(archivo_creado,columnas,filas);
+		}else if (pregunta.equalsIgnoreCase("Defecto")) {
+			File f =new File("Matriz.txt");
+			File f2 = new File("MatrizPorDefecto.txt");
+			gc.escribirMatrizporDefecto(f,f2,columnas, filas);
+		}
+		
 	}
 	public File escribirMatriz(File x,int n,int m ) {
 		PrintStream ps = null;
@@ -33,6 +46,22 @@ public class GuardarMatrizenFichero {
 		}
 		return x;
 	}
+	public File escribirMatrizporDefecto(File x,File v,int n,int m) {
+		int MatrizDefecto  [][] = new int [n][m];
+		try {
+			Scanner scan = new Scanner(v);
+			PrintStream ps = new PrintStream(x);
+			while (scan.hasNext()) {
+				String texto  =scan.nextLine();
+				System.out.println(texto);
+				ps.println(texto+" ");
+			}
+		} catch (Exception e) {
+			System.out.println("Error! : "+e);
+			e.printStackTrace();
+		}
+		return x;
+	}
 	public int GenerarNumAleatorio (){
 		Random r =new Random();
 		int numAleatorio  = r.nextInt(20);
@@ -46,5 +75,10 @@ public class GuardarMatrizenFichero {
 			}
 			System.out.println();
 		}
+	}
+	public static String recogerCadena(){
+		Scanner scan = new Scanner(System.in);
+		String palabra = scan.next();
+		return palabra;
 	}
 }
